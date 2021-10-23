88503822e6d3d3e46f246e731d09e71a0ddc2f27
Balamurugan Thayalan <32746133+balamt@users.noreply.github.com>
2021-10-23 23:25:35 +0530

    Used Runtime process to call the chcp 65001, which allow ANSI color & emoji to print in win console.

--- src/main/java/com/coderstea/squidgame/SquidGameJava.java
+++ src/main/java/com/coderstea/squidgame/SquidGameJava.java
@@ -4,18 +4,28 @@ import com.coderstea.squidgame.games.RedLightGreenLight;
 
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 public class SquidGameJava {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int gameNo = 0;
     try {
       do {
-        System.out.println("Starting Squid Game. Please select a Game to play");
-        System.out.println("1. Red Light Green Light");
-        System.out.println("5. Don't Want to Play");
-        System.out.println("Your Choice: ");
-        gameNo = sc.nextInt();
-        startGame(gameNo);
+        // This will allow the cmd to use the ANSI colors and emoji.
+        // check README.md for the known issues.
+        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "chcp", "65001").inheritIO();
+        Process p = pb.start();
+        p.waitFor();
+
+        if(!p.isAlive()) {
+          System.out.println("Starting Squid Game. Please select a Game to play");
+          System.out.println("1. Red Light Green Light");
+          System.out.println("5. Don't Want to Play");
+          System.out.println("Your Choice: ");
+          gameNo = sc.nextInt();
+          startGame(gameNo);
+        }
       } while (gameNo > 5);
     } catch (Exception e){
       e.printStackTrace();
