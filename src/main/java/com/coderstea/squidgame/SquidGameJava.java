ed001691f113bba87d58b8f3df67dfde691dbc18
Balamurugan Thayalan <32746133+balamt@users.noreply.github.com>
2021-10-24 17:01:55 +0530

    Added condition to check OS is Windows then execute cmd.exe

--- src/main/java/com/coderstea/squidgame/SquidGameJava.java
+++ src/main/java/com/coderstea/squidgame/SquidGameJava.java
@@ -1,31 +1,32 @@
 package com.coderstea.squidgame;
 
 import com.coderstea.squidgame.games.RedLightGreenLight;
+import com.coderstea.squidgame.util.Util;
 
 import java.util.Scanner;
 
 import static java.lang.System.exit;
 
 public class SquidGameJava {
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int gameNo = 0;
     try {
       do {
-        // This will allow the cmd to use the ANSI colors and emoji.
+        // This will allow the cmd to use the ANSI colors and emoji for Windows OS only
         // check README.md for the known issues.
-        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "chcp", "65001").inheritIO();
-        Process p = pb.start();
-        p.waitFor();
-
-        if(!p.isAlive()) {
+          if(Util.isWindows()) {
+            ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "chcp", "65001").inheritIO();
+            Process cmdProcess = pb.start();
+            cmdProcess.waitFor();
+          }
           System.out.println("Starting Squid Game. Please select a Game to play");
           System.out.println("1. Red Light Green Light");
           System.out.println("5. Don't Want to Play");
           System.out.println("Your Choice: ");
           gameNo = sc.nextInt();
           startGame(gameNo);
-        }
       } while (gameNo > 5);
     } catch (Exception e){
       e.printStackTrace();
--- src/main/java/com/coderstea/squidgame/util/Util.java
+++ src/main/java/com/coderstea/squidgame/util/Util.java
@@ -1,11 +1,17 @@
 package com.coderstea.squidgame.util;
 
 public class Util {
+  private static String OS = System.getProperty("os.name").toLowerCase();
+
   public static void sleep(int time) {
     try {
       Thread.sleep(time);
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
+  }
+
+  public static boolean isWindows() {
+    return (OS.indexOf("win") >= 0);
   }
 }
